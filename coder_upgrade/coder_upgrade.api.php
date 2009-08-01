9be7ebcc9c71d627e99307ed5f20ee9fc8983152
solotandem <solotandem>
2009-08-01 12:55:09 +0000

    Revise user interface using new D7 form elements; implement hook_upgrades; revise help text.

---
+++ coder_upgrade/coder_upgrade.api.php
@@ -0,0 +1,16 @@
+<?php
+// $Id: coder_upgrade.api.php,v 1.1 2009-08-01 12:55:09 solotandem Exp $
+
+/**
+ * @file
+ * The api documentation for the coder_upgrade module.
+ *
+ * Copyright 2008-9 by Jim Berry ("solotandem", http://drupal.org/user/240748)
+ */
+
+/**
+ * Documentation of hook_upgrades.
+ */
+function hook_upgrades()  {
+  //
+}
